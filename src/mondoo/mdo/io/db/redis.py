abbe59e7fbf5020c3f7400605b1046e6d5d73c28
breezonto <fzb1122@outlook.com>
2026-08-18 23:09:01 +0800

    #refactor: #refactor: move redis configuration to configurator

--- src/mondoo/mdo/io/db/redis.py
+++ src/mondoo/mdo/io/db/redis.py
@@ -1,13 +1,10 @@
+from mondoo.configurator import REDIS_HOST, REDIS_PORT, REDIS_DB
+
 from typing import Optional, Dict
 
 import redis
 import os
 import json
-
-
-REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-REDIS_PORT = os.getenv('REDIS_PORT', 6379)
-REDIS_DB   = list(set(map(int, os.getenv('REDIS_DB').split(','))))
 
 
 rh = redis.Redis(
